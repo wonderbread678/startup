800bb2317fe1ae67b93ffe5c27332d16517d5c86
wonderbread678 <wonderbread678@gmail.com>
2025-10-07 13:10:32 -0600

    fixed app files

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body bg-light text-light">
+      <header className="container-fluid">
+        <nav>
+        <a className="navbar-brand" href="#"><h1>Koob<sup>&reg;</sup></h1></a>
+            <menu className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link active" style={{color:"white"}} href="index.html">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" style={{color:"white"}} href="Entry_upload.html">Upload</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" style={{color:"white"}} href="List.html">List</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" style={{color:"white"}} href="Profile.html">Profile</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" style={{color:"white"}} href="About.html">About</a>
+                </li>
+            </menu>
+        </nav>
+      </header>
+
+      <main>App components go here</main>
+
+        <footer style={{color: "lightgray"}}>
+            <div className="container-fluid" id="footerDiv">
+                <span className="text-reset">Author: Ethan Nielsen</span>
+                <a id="gitFooter" className="text-reset" href="https://github.com/wonderbread678/startup">GitHub</a>
+            </div>
+        </footer>
+    </div>
+  );
 }
