591d20d91a033a31a971cee6f0f037892aff12e4
wonderbread678 <wonderbread678@gmail.com>
2025-10-09 13:23:47 -0600

    troubleshooting, fixed a couple more

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -5,7 +5,7 @@ export function About() {
   return (
     <main className="aboutMain">
       <div>
-        <img src="https://thumbs.dreamstime.com/b/row-vintage-books-background-web-banner-free-copy-space-reading-education-literature-book-fair-concept-170940371.jpg" alt="aboutBanner" />
+        <img className="aboutImg" src="https://thumbs.dreamstime.com/b/row-vintage-books-background-web-banner-free-copy-space-reading-education-literature-book-fair-concept-170940371.jpg" alt="aboutBanner" />
             <div id="about">
                 <h2>~ About us ~</h2>
                 Koob is a fresh, innovative take on media rating site, allowing the user essentially complete freedom to upload titles and works and compile them into lists, whether books, poetry, comics, and more. The ease of upload allows any user to create and rate entries immediately after reading them, as well as compile a comprehensive list of anything they might have already read, giving users a hub for their literature and a way to track and rate their literary journey. Unlike traditional review sites that limit what can be submitted or emphasize only popular titles, Koob offers complete freedom for uploads, ensuring that every piece of literature has a place, no matter how niche.
