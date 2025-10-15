d93cd4669b55ce74d9e1e97d4d92bfc4850b5201
wonderbread678 <wonderbread678@gmail.com>
2025-10-14 20:08:39 -0600

    authenticated done

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -4,13 +4,19 @@ import { useNavigate } from 'react-router-dom'
 
 export function Authenticated(props){
 
+    function logout(){
+        localStorage.removeItem('userName')
+        props.onLogout();
+    }
+
     return (<main className='main'>
         <div className='body'>
             <h1>Welcome to Koob</h1>
+            <h2>{props.userName}</h2>
         </div>
         <button onClick={() => useNavigate('/entry_upload')}>Entry upload</button>
         <button onClick={() => useNavigate('/list')}>View lists</button>
         <button onClick={() => logout()}>Logout</button>
     </main>
-    )
+    );
 }
