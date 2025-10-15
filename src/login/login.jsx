c8624de5d64b2c10641043f29fd382aded3654ed
wonderbread678 <wonderbread678@gmail.com>
2025-10-14 20:21:56 -0600

    started login.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -11,6 +11,11 @@ export function Login({userName, authState, onAuthChange}) {
     <main className="main">
       <div className="body">
         {authState !== AuthState.Unknown && <h1>Welcome to Koob</h1>}
+        {authState === AuthState.Authenticated && (<Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)}/>
+      )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated userName={loginUserName} onLogin={() => onAuthChange(loginUserName, AuthState.Authenticated)}/>
+        )}
       </div>
     </main>
   );
