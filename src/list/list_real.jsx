6ee46ee4444ddfd3911bfb2b36f24b839240d1cb
wonderbread678 <wonderbread678@gmail.com>
2025-10-16 21:13:38 -0600

    tried creating new jsx file for the list, temporarily

---
+++ src/list/list_real.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './list.css'
+
+export function List(props){
+
+    function entryItem(){
+
+    }
+
+    function listCatagory(){
+
+    }
+
+    function handleCatagory(){
+
+    }
+    
+    function handleEdit(){
+
+    }
+
+    function handleDelete(){
+        
+    }
+
+    return(
+        <main>
+
+        </main>
+    );
+}
