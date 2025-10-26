65bf36be9fda018101063d76ba180e2d84328991
wonderbread678 <wonderbread678@gmail.com>
2025-10-26 13:30:24 -0600

    updating css

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -16,9 +16,11 @@ export function Authenticated(props){
         <main className='main'>
             <div className='body'>
                 <h2 id="userDisplay">{props.userName}</h2>
-                <button className="btn btn-primary" id="primaryButton" onClick={() => navigate('/entry_upload')}>Upload Entry</button>
-                <button className="btn btn-primary" id="primaryButton" onClick={() => navigate('/list')}>View lists</button>
-                <button className="btn btn-primary" id="secondaryButton" onClick={() => logout()}>Logout</button>
+                <div id="buttonContainer">
+                    <button className="btn btn-primary" id="primaryButton" onClick={() => navigate('/entry_upload')}>Upload Entry</button>
+                    <button className="btn btn-primary" id="primaryButton" onClick={() => navigate('/list')}>View lists</button>
+                    <button className="btn btn-primary" id="secondaryButton" onClick={() => logout()}>Logout</button>
+                </div>
                 <h3 id="description">See what users are up to!</h3>
                 <div className='body' id="partyBox" style={{border:"gold 10px groove"}}>
                     <h3 id="thirdPartyTitle">Uploads: </h3>
