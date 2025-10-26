e7449b7e4d7a49ef4466afc7f4a7a3eda5bd2756
wonderbread678 <wonderbread678@gmail.com>
2025-10-26 13:21:21 -0600

    fixing css

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useNavigate, NavLink } from 'react-router-dom'
-import './login.css'
-
+import { useNavigate } from 'react-router-dom'
+import './authenticated.css'
 
 export function Authenticated(props){
     const navigate = useNavigate();
@@ -17,12 +16,12 @@ export function Authenticated(props){
         <main className='main'>
             <div className='body'>
                 <h2>{props.userName}</h2>
-                <button className="btn btn-primary" onClick={() => navigate('/entry_upload')}>Upload Entry</button>
-                <button className="btn btn-primary" onClick={() => navigate('/list')}>View lists</button>
-                <button className="btn btn-primary" onClick={() => logout()}>Logout</button>
-                <h3>See what users are up to!</h3>
-                <div className='body' style={{border:"gold 10px groove"}}>
-                    <h3>Uploads: </h3>
+                <button className="btn btn-primary" id="primaryButton" onClick={() => navigate('/entry_upload')}>Upload Entry</button>
+                <button className="btn btn-primary" id="primaryButton" onClick={() => navigate('/list')}>View lists</button>
+                <button className="btn btn-primary" id="secondaryButton" onClick={() => logout()}>Logout</button>
+                <h3 id="description">See what users are up to!</h3>
+                <div className='body' id="partyBox" style={{border:"gold 10px groove"}}>
+                    <h3 id="thirdPartyTitle">Uploads: </h3>
                     <span>Ethan created an entry!</span>
                     <span>Ethan created an entry!</span>
                     <span>Caseoh created an entry!</span>
