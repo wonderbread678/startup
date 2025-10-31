b12bbbec085330c7010d5e5eb3ee5d079085be08
wonderbread678 <wonderbread678@gmail.com>
2025-10-30 21:20:21 -0600

    adding backend stuff, working from simon help doc

--- service/index.js
+++ service/index.js
@@ -5,13 +5,42 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const uuid = require('uuid');
 
+app.use(express.json());
+
+let entries = [];
+let profiles = [];
+
+let apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
-app.get('*', (_req, res) => {
-  res.send({ msg: 'Simon service' });
-});
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
 });
