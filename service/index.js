164864483c830dcb97ea7db66c095522c311c9f4
wonderbread678 <wonderbread678@gmail.com>
2025-11-03 23:47:37 -0700

    troubleshooting entry upload with TA

--- service/index.js
+++ service/index.js
@@ -14,10 +14,10 @@ app.use(cookieParser());
 let entries = [];
 let users = [];
 
-let apiRouter = express.Router();
+var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
@@ -75,7 +75,7 @@ apiRouter.get('/list', verifyAuth, (_req, res) => {
 });
 
 // Submit entry
-apiRouter.post('/entries', verifyAuth, async (req, res) => {
+apiRouter.post('/entries', async (req, res) => {
     const newEntry = {
         id: req.body.id,
         title: req.body.title,
@@ -89,7 +89,8 @@ apiRouter.post('/entries', verifyAuth, async (req, res) => {
         listName: req.body.listName
     }
     entries = updateEntries(newEntry);
-    res.send(entries);
+    res.send({entryName: newEntry.title});
+    return;
 });
 
 // Delete entry
@@ -152,54 +153,54 @@ function updateEntries(newEntry) {
 
 
 
-// MongoDB stuff //
-const { MongoClient } = require('mongodb');
-const config = require('./dbConfig.json');
+// // MongoDB stuff //
+// const { MongoClient } = require('mongodb');
+// const config = require('./dbConfig.json');
 
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+// const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
-// Connect to the database cluster
-const client = new MongoClient(url);
-const db = client.db('rental');
-const collection = db.collection('house');
+// // Connect to the database cluster
+// const client = new MongoClient(url);
+// const db = client.db('rental');
+// const collection = db.collection('house');
 
-async function main() {
-  try {
-    // Test that you can connect to the database
-    await db.command({ ping: 1 });
-    console.log(`DB connected to ${config.hostname}`);
-  } catch (ex) {
-    console.log(`Connection failed to ${url} because ${ex.message}`);
-    process.exit(1);
-  }
+// async function main() {
+//   try {
+//     // Test that you can connect to the database
+//     await db.command({ ping: 1 });
+//     console.log(`DB connected to ${config.hostname}`);
+//   } catch (ex) {
+//     console.log(`Connection failed to ${url} because ${ex.message}`);
+//     process.exit(1);
+//   }
 
-  try {
-    // Insert a document
-    const house = {
-      name: 'Beachfront views',
-      summary: 'From your bedroom to the beach, no shoes required',
-      property_type: 'Condo',
-      beds: 1,
-    };
-    await collection.insertOne(house);
+//   try {
+//     // Insert a document
+//     const house = {
+//       name: 'Beachfront views',
+//       summary: 'From your bedroom to the beach, no shoes required',
+//       property_type: 'Condo',
+//       beds: 1,
+//     };
+//     await collection.insertOne(house);
 
-    // Query the documents
-    const query = { property_type: 'Condo', beds: { $lt: 2 } };
-    const options = {
-      sort: { name: -1 },
-      limit: 10,
-    };
-    const cursor = collection.find(query, options);
-    const rentals = await cursor.toArray();
-    rentals.forEach((i) => console.log(i));
+//     // Query the documents
+//     const query = { property_type: 'Condo', beds: { $lt: 2 } };
+//     const options = {
+//       sort: { name: -1 },
+//       limit: 10,
+//     };
+//     const cursor = collection.find(query, options);
+//     const rentals = await cursor.toArray();
+//     rentals.forEach((i) => console.log(i));
 
-    // Delete documents
-    await collection.deleteMany(query);
-  } catch (ex) {
-    console.log(`Database (${url}) error: ${ex.message}`);
-  } finally {
-    await client.close();
-  }
-}
+//     // Delete documents
+//     await collection.deleteMany(query);
+//   } catch (ex) {
+//     console.log(`Database (${url}) error: ${ex.message}`);
+//   } finally {
+//     await client.close();
+//   }
+// }
 
-main();
+// main();
