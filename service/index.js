2913b2772704cf7c64aa8a661654532328abfd97
wonderbread678 <wonderbread678@gmail.com>
2025-11-06 20:50:33 -0700

    fixed bug that wouldnt let images send

--- service/index.js
+++ service/index.js
@@ -8,7 +8,7 @@ const uuid = require('uuid');
 const authCookieName = 'token';
 app.use(cookieParser());
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 app.use(express.static('public'));
 
