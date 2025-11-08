4e317d45908d0ec2c578e06703e25f07e78e743e
wonderbread678 <wonderbread678@gmail.com>
2025-11-07 23:26:54 -0700

    naming mismatch so that users couldnt log back in after creating an account

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -28,7 +28,7 @@ export function Unauthenticated(props) {
     async function loginOrCreate(endpoint) {
       const response = await fetch(endpoint, {
         method: 'post',
-        body: JSON.stringify({ email: userName, password: password }),
+        body: JSON.stringify({ userName: userName, password: password }),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
