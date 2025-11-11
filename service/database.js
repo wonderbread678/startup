6797ac6c94bc0d4f7a26e114ff4678f9d7defd8a
wonderbread678 <wonderbread678@gmail.com>
2025-11-10 22:16:29 -0700

    copied over user database functions from simon

---
+++ service/database.js
@@ -0,0 +1,41 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('koob');
+const entriesCollection = db.collection('entries');
+const listsCollection = db.collection('lists');
+const userCollection = db.collection('users');
+const profilesCollection = db.collection('profiles');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+
+
+
+main();
