d896262f0661a7c88926ebcc1e2e97b3c564c4c3
wonderbread678 <wonderbread678@gmail.com>
2025-11-16 12:56:38 -0700

    adding more to upload messenger

--- service/uploadMessenger.js
+++ service/uploadMessenger.js
@@ -1,6 +1,8 @@
 const { WebSocketServer } = require('ws');
 
-function uploadMessenger(httpServer){
+let socketServer;
+
+function peerProxy(httpServer){
     const socketServer = new WebSocketServer({ server: httpServer });
     socketServer.on('connection', (socket => {
         socket.isAlive = true;
@@ -14,9 +16,24 @@ function uploadMessenger(httpServer){
             if (client.isAlive === false){
                 return client.terminate();
             }
-
             client.isAlive = false;
             client.ping();
         });
     }, 10000);
 }
+
+function uploadMessenger(userName, title){
+    const message = JSON.stringify({
+        user: userName,
+        title: title,
+        date: new Date().toISOString()
+    });
+
+    socketServer.clients.forEach((client) => {
+        if (client !== socket && client.readyState === client.OPEN) {
+            client.send(message);
+        }
+    });
+}
+
+module.exports = { peerProxy, uploadMessenger };
