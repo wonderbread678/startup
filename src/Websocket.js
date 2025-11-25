60c28a4ee4bd18107031efb56ce389946db17f0c
wonderbread678 <wonderbread678@gmail.com>
2025-11-24 16:53:36 -0800

    copied ChatClient class from Simon to edit

--- src/Websocket.js
+++ src/Websocket.js
@@ -1,6 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+
+class ChatClient {
+  observers = [];
+  connected = false;
+
+  constructor() {
+    // Adjust the webSocket protocol to what is being used for HTTP
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+
+    // Display that we have opened the webSocket
+    this.socket.onopen = (event) => {
+      this.notifyObservers('system', 'websocket', 'connected');
+      this.connected = true;
+    };
+
+    // Display messages we receive from our friends
+    this.socket.onmessage = async (event) => {
+      const text = await event.data.text();
+      const chat = JSON.parse(text);
+      this.notifyObservers('received', chat.name, chat.msg);
+    };
+
+    // If the webSocket is closed then disable the interface
+    this.socket.onclose = (event) => {
+      this.notifyObservers('system', 'websocket', 'disconnected');
+      this.connected = false;
+    };
+  }
+
+  // Send a message over the webSocket
+  sendMessage(name, msg) {
+    this.notifyObservers('sent', 'me', msg);
+    this.socket.send(JSON.stringify({ name, msg }));
+  }
+
+  addObserver(observer) {
+    this.observers.push(observer);
+  }
+
+  notifyObservers(event, from, msg) {
+    this.observers.forEach((h) => h({ event, from, msg }));
+  }
+}
+
 function Message ({webSocket}){
 
     function sendMsg(){
@@ -23,3 +68,4 @@ function Uploads({ webSocket }){
         </div>
     ));
 }
+
